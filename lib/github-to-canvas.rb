03b79e74f5cc443a8f84e9dc87fba92d7f07455d
maxwellbenton <maxwellbenton@gmail.com>
2020-05-16 17:17:26 -0400

    add --dry-run functionality

--- lib/github-to-canvas.rb
+++ lib/github-to-canvas.rb
@@ -1,9 +1,10 @@
 require_relative './github-to-canvas/create-canvas-lesson'
 require_relative './github-to-canvas/version'
+require_relative './github-to-canvas/github_interface'
 
 class GithubToCanvas
 
-  def initialize(mode:, course:, filepath:Dir.pwd, branch:'master', name:File.basename(Dir.getwd), type:"page")
+  def initialize(mode:, course:, filepath:Dir.pwd, branch:'master', name:File.basename(Dir.getwd), type:"page", dry:false)
     if mode == 'version'
       puts VERSION
       return
@@ -11,7 +12,7 @@ class GithubToCanvas
 
     if mode == 'create'
       puts "github-to-canvas will now create a Canvas lesson based on the current repo"
-      CreateCanvasLesson.new(course, filepath, branch, name, type)
+      CreateCanvasLesson.new(course, filepath, branch, name, type, dry)
     end
   end
 
--- lib/github-to-canvas/create-canvas-lesson.rb
+++ lib/github-to-canvas/create-canvas-lesson.rb
@@ -2,18 +2,21 @@ require 'redcarpet'
 require 'rest-client'
 require 'json'
 require 'yaml'
-require 'byebug'
+require_relative './github_interface'
 
 class CreateCanvasLesson
 
-  def initialize(course, filepath, branch, name, type)
+  def initialize(course, filepath, branch, name, type, dry_run)
+    puts "Dry: #{dry_run}"
     @course = course
     @filepath = filepath
     @branch = branch
-    @name = name.split(/[ -]/).map(&:capitalize).join(' ')
+    @name = name.split(/[- _]/).map(&:capitalize).join(' ')
     @type = type
     @renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options={tables: true, autolink: true, fenced_code_blocks: true})
     @original_readme = File.read("#{filepath}/README.md")
+    @dry_run = dry_run
+    @response = {'page_id' => '<the canvas page id>', 'html_url' => '<the canvsas lesson URL>'} if dry_run
     if !@original_readme
       puts 'README.md not found in current directory. Exiting...'
       abort
@@ -22,81 +25,65 @@ class CreateCanvasLesson
   end
 
   def create_canvas_lesson
-    git_co_branch
-    git_pull
+    get_updated_repo
+    fix_local_images
+    convert_to_html
+    submit_to_canvas
+    update_remote_repo
+  end
+
+  def get_updated_repo
+    GithubInterface.git_co_branch(@filepath, @branch)
+    GithubInterface.git_pull(@filepath, @branch)
+  end
+
+  def fix_local_images
     @raw_remote_url = set_raw_image_remote_url
     adjust_local_markdown_images
     adjust_local_html_images
+  end
+
+  def convert_to_html
     @new_readme = @renderer.render(@original_readme)
     write_to_html
-    @response = push_to_canvas
-    if @response.code != 200
-      puts "Canvas push failed. #{@response.code} status code returned "
-    end
-    @response = JSON.parse(@response.body)
-    create_canvas_dotfile
-    commit_canvas_dotfile
-    git_push
   end
 
-  def cd_into_and(command)
-    cmd = "cd #{@filepath} && #{command}"
-    puts cmd
-    `#{cmd}`
-  end
-
-  def git_co_branch
-    cmd = "git co #{@branch}"
-    branch = cd_into_and(cmd)
-    if branch.to_s.strip.empty?
-      puts "#{b@ranch} branch not found. Exiting..."
-      abort
-    end
-  end
-
-  def git_pull
-    if @branch != 'master'
-      cmd = "git pull #{@branch}"
+  def submit_to_canvas
+    if @dry_run
+      puts 'DRY RUN: Skipping push to Canvas'
     else
-      cmd =  "git pull"
+      @response = push_to_canvas
+      if @response.code != 200
+        puts "Canvas push failed. #{@response.code} status code returned "
+      end
+      @response = JSON.parse(@response.body)
     end
-    puts "git pulling latest version of #{@branch}"
-    cd_into_and(cmd)
   end
 
-  def git_remote
-    cmd = "git config --get remote.origin.url"
-    cd_into_and(cmd)
-  end
-
-  def git_add(file)
-    cmd = "git add #{file}"
-    puts "git adding #{file}"
-    cd_into_and(cmd)
-  end
-
-  def git_commit(message)
-    cmd = "git commit -m '#{message}'"
-    puts "git commit: '#{message}'"
-    cd_into_and(cmd)
-  end
-
-  def git_push
-    cmd = "git push"
-    puts "git pushing #{@branch}"
-    cd_into_and(cmd)
+  def update_remote_repo
+    if @dry_run
+      puts 'DRY RUN: Skipping push to GitHub'
+      puts 'If this were live, the following hash would be converted into a .canvas dotfile:'
+      puts handle_existing_canvas_dotfile
+    else
+      create_canvas_dotfile
+      commit_canvas_dotfile
+      GithubInterface.git_push(@filepath, @branch)
+    end
   end
   
   def set_raw_image_remote_url
-    remote = git_remote
+    remote = GithubInterface.git_remote(@filepath)
     remote.gsub!("git@github.com:","https://raw.githubusercontent.com/")
     remote.gsub!(/.git$/,"")
     remote.strip!
   end
 
+  private
+  
   def adjust_local_markdown_images
     @original_readme.gsub!(/\!\[.+\]\(.+\)/) {|image|
-      if !image.match('amazonaws.com') && !image.match('https://')
+      if !image.match('amazonaws.com') && !image.match('https://') && !image.match('youtube')
         image.gsub!(/\(.+\)/) { |path|
           path.delete_prefix!("(")
           path.delete_suffix!(")")
@@ -109,9 +96,11 @@ class CreateCanvasLesson
 
   def adjust_local_html_images
     @original_readme.gsub!(/src=\"[\s\S]*?" /) { |img|
-      img.gsub!(/\"/, "")
-      img.gsub!(/src=/, '')
-      img.strip!
+      if !image.match('amazonaws.com') && !image.match('https://') && !image.match('youtube')
+        img.gsub!(/\"/, "")
+        img.gsub!(/src=/, '')
+        img.strip!
+      end
       'src="' + @raw_remote_url + '/master/' + img + '"'
     }
   end
@@ -140,17 +129,28 @@ class CreateCanvasLesson
     })
   end
 
+  def handle_existing_canvas_dotfile
+    if File.file?(".canvas")
+      canvas_data = YAML.load(File.read(".canvas"))
+      canvas_data[:page_id] << @response['page_id'] if !canvas_data[:page_id].include? @response['page_id']
+      canvas_data[:course_id] << @course.to_i if !canvas_data[:course_id].include? @course.to_i
+      canvas_data[:canvas_url] << @response['html_url'] if !canvas_data[:canvas_url].include? @response['html_url']
+    else
+      canvas_data = {
+        page_id: [@response['page_id']],
+        course_id: [@course.to_i],
+        canvas_url: [@response['html_url']]
+      }
+    end
+    canvas_data
+  end
+
   def create_canvas_dotfile
-    canvas_data = {
-      page_id: @response['page_id'],
-      course_id: @course.to_i,
-      canvas_url: @response['html_url']
-    }
-    File.write("#{@filepath}/.canvas", canvas_data.to_yaml)
+    File.write("#{@filepath}/.canvas", handle_existing_canvas_dotfile.to_yaml)
   end
 
   def commit_canvas_dotfile
-    git_add('.canvas')
-    git_commit('AUTO: add .canvas file after migration')
+    GithubInterface.git_add(@filepath, '.canvas')
+    GithubInterface.git_commit(@filepath, 'AUTO: add .canvas file after migration')
   end
 end
