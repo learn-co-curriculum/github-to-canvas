1c78d5ef140d2ff476ff1285fa2c4f7084f056d9
maxwellbenton <maxwellbenton@gmail.com>
2020-05-17 09:19:59 -0400

    separate build_payload method, finish update_existing_lesson

--- lib/github-to-canvas/canvas_interface.rb
+++ lib/github-to-canvas/canvas_interface.rb
@@ -3,11 +3,11 @@ require 'rest-client'
 require 'byebug'
 class CanvasInterface
 
-  def self.submit_to_canvas(course, type, name, readme, dry_run = false)
+  def self.submit_to_canvas(course_id, type, name, readme, dry_run = false)
     if dry_run
       puts 'DRY RUN: Skipping push to Canvas'
     else
-      response = self.push_to_canvas(course, type, name, readme)
+      response = self.push_to_canvas(course_id, type, name, readme)
       if ![200, 201].include? response.code
         puts "Canvas push failed. #{response.code} status code returned "
         abort
@@ -16,30 +16,35 @@ class CanvasInterface
     end
   end
 
-  def self.push_to_canvas(course, type, name, readme)
-    url = "#{ENV['CANVAS_API_PATH']}/courses/#{course}/#{type}s"
-    if type == "assignment"
-      payload = {
-        'assignment[name]' => name,
-        'assignment[description]' => readme
-      }
-    else
-      payload = {
-        'wiki_page[title]' => name,
-        'wiki_page[body]' => readme,
-        'wiki_page[editing_roles]' => "teachers" 
-      }
-    end
+  def self.push_to_canvas(course_id, type, name, new_readme)
+    url = "#{ENV['CANVAS_API_PATH']}/courses/#{course_id}/#{type}s"
+    payload = self.build_payload(type, name, new_readme)
 
     RestClient.post(url, payload, headers={
       "Authorization" => "Bearer #{ENV['CANVAS_API_KEY']}"
     })
   end
 
-  def self.update_existing_lesson(course_id, page_id, type, name, new_readme, dry_run)
-    url = "#{ENV['CANVAS_API_PATH']}/courses/#{course}/#{type}s/#{page_id}"
+  def self.update_existing_lesson(course_id, id, type, name, new_readme, dry_run)
+    url = "#{ENV['CANVAS_API_PATH']}/courses/#{course_id}/#{type}s/#{id}"
+    payload = self.build_payload(type, name, new_readme)
     RestClient.put(url, payload, headers={
       "Authorization" => "Bearer #{ENV['CANVAS_API_KEY']}"
     })
+  end
+
+  def self.build_payload(type, name, new_readme)
+    if type == "assignment"
+      payload = {
+        'assignment[name]' => name,
+        'assignment[description]' => new_readme
+      }
+    else
+      payload = {
+        'wiki_page[title]' => name,
+        'wiki_page[body]' => new_readme,
+        'wiki_page[editing_roles]' => "teachers" 
+      }
+    end
   end
 end
