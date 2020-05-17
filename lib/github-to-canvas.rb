6e15c482d2502cc8d42bc96aca5527bdac14b86e
maxwellbenton <maxwellbenton@gmail.com>
2020-05-17 00:06:38 -0400

    add mode for align

--- lib/github-to-canvas.rb
+++ lib/github-to-canvas.rb
@@ -1,6 +1,11 @@
-require_relative './github-to-canvas/create-canvas-lesson'
-require_relative './github-to-canvas/version'
+require_relative './github-to-canvas/create_canvas_lesson'
+require_relative './github-to-canvas/update_canvas_lesson'
+require_relative './github-to-canvas/repository_converter'
 require_relative './github-to-canvas/github_interface'
+require_relative './github-to-canvas/canvas_interface'
+require_relative './github-to-canvas/canvas_dotfile'
+require_relative './github-to-canvas/version'
+
 
 class GithubToCanvas
 
@@ -13,6 +18,11 @@ class GithubToCanvas
     if mode == 'create'
       puts "github-to-canvas will now create a Canvas lesson based on the current repo"
       CreateCanvasLesson.new(course, filepath, branch, name, type, dry)
+    end
+
+    if mode == 'align'
+      puts "github-to-canvas will now align any existing Canvas lessons based on the current repo. NOTE: .canvas file must be present"
+      UpdateCanvasLesson.new(filepath, branch, name, type, dry)
     end
   end
 
