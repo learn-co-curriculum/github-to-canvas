ffc07b917234579f305b75a203ee415e29dd498e
maxwellbenton <maxwellbenton@gmail.com>
2020-06-13 07:57:42 -0400

    remove_header_and_footer, and flexible lesson[:type]

--- lib/github-to-canvas/update_canvas_lesson.rb
+++ lib/github-to-canvas/update_canvas_lesson.rb
@@ -1,24 +1,24 @@
 class UpdateCanvasLesson
 
-  def initialize(filepath, branch, name, type, dry_run, fis_links, remove_header)
+  def initialize(filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
     name = name.split(/[- _]/).map(&:capitalize).join(' ')
     readme = File.read("#{filepath}/README.md")
     if !readme
       puts 'README.md not found in current directory. Exiting...'
       abort
     end
-    update_canvas_lesson(readme, filepath, branch, name, type, dry_run, fis_links, remove_header)
+    update_canvas_lesson(readme, filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
   end
 
-  def update_canvas_lesson(readme, filepath, branch, name, type, dry_run, fis_links, remove_header)
+  def update_canvas_lesson(readme, filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
     GithubInterface.get_updated_repo(filepath, branch)
-    new_readme = RepositoryConverter.convert(filepath, readme, branch, remove_header)
+    new_readme = RepositoryConverter.convert(filepath, readme, branch, remove_header_and_footer)
     if fis_links
       new_readme = RepositoryConverter.add_fis_links(filepath, new_readme)
     end
     canvas_data = CanvasDotfile.read_canvas_data
     canvas_data[:lessons].each { |lesson|
-      CanvasInterface.update_existing_lesson(lesson[:course_id], lesson[:id], type, name, new_readme, dry_run)
+      CanvasInterface.update_existing_lesson(lesson[:course_id], lesson[:id], lesson[:type], name, new_readme, dry_run)
     }
   end
 
