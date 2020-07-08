6f03d80e28b69c8b0599d99e8ca7d9bf5e6d7967
maxwellbenton <maxwellbenton@gmail.com>
2020-07-08 14:21:01 -0400

    updates creation process to make saving optional, handle different markdown files

--- lib/github-to-canvas/create_canvas_lesson.rb
+++ lib/github-to-canvas/create_canvas_lesson.rb
@@ -1,29 +1,32 @@
 class CreateCanvasLesson
 
-  def initialize(course, filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
+  def initialize(course, filepath, file_to_convert, branch, name, type, save_to_github, fis_links, remove_header_and_footer)
     name = name.split(/[- _]/).map(&:capitalize).join(' ')
-    original_readme = File.read("#{filepath}/README.md")
-    if !original_readme
-      puts 'README.md not found in current directory. Exiting...'
+    begin
+      markdown = File.read("#{filepath}/#{file_to_convert}")
+    rescue
+      puts "#{file_to_convert} not found in current directory. Exiting..."
       abort
     end
-    create_canvas_lesson(original_readme, course, filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
+    create_canvas_lesson(markdown, course, filepath, branch, name, type, save_to_github, fis_links, remove_header_and_footer)
   end
 
-  def create_canvas_lesson(readme, course, filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
+  def create_canvas_lesson(markdown, course, filepath, branch, name, type, save_to_github, fis_links, remove_header_and_footer)
     GithubInterface.get_updated_repo(filepath, branch)
-    new_readme = RepositoryConverter.convert(filepath, readme, branch, remove_header_and_footer)
+    new_html = RepositoryConverter.convert(filepath, markdown, branch, remove_header_and_footer)
     if fis_links
-      new_readme = RepositoryConverter.add_fis_links(filepath, new_readme)
+      new_html = RepositoryConverter.add_fis_links(filepath, new_html)
     end
-    response = CanvasInterface.submit_to_canvas(course, type, name, new_readme, dry_run)
-    if dry_run
-      puts 'DRY RUN: Skipping dotfile creation and push to GitHub'
-    else
+    response = CanvasInterface.submit_to_canvas(course, type, name, new_html)
+    if save_to_github
+      puts 'Creating .canvas file'
       CanvasDotfile.update_or_create(filepath, response, course, type)
+      puts 'Commiting .canvas file'
       CanvasDotfile.commit_canvas_dotfile(filepath)
+      puts 'Pushing .canvas file'
       GithubInterface.git_push(filepath, branch)
     end
+    puts "Canvas lesson created. Lesson available at #{response['html_url']}."
   end
 
 end
