8f3105e117542063b1d96bec691a46b974a4a51d
maxwellbenton <maxwellbenton@gmail.com>
2020-07-08 14:21:39 -0400

    update align process to handle manual course/id entry

--- lib/github-to-canvas/update_canvas_lesson.rb
+++ lib/github-to-canvas/update_canvas_lesson.rb
@@ -1,25 +1,34 @@
 class UpdateCanvasLesson
 
-  def initialize(filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
+  def initialize(course, filepath, file_to_convert, branch, name, type, save_to_github, fis_links, remove_header_and_footer, only_update_content, id)
     name = name.split(/[- _]/).map(&:capitalize).join(' ')
-    readme = File.read("#{filepath}/README.md")
-    if !readme
-      puts 'README.md not found in current directory. Exiting...'
+    begin
+      markdown = File.read("#{filepath}/#{file_to_convert}")
+    rescue
+      puts "#{file_to_convert} not found in current directory. Exiting..."
       abort
     end
-    update_canvas_lesson(readme, filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
+    update_canvas_lesson(course, markdown, filepath, branch, name, type, save_to_github, fis_links, remove_header_and_footer, only_update_content, id)
   end
 
-  def update_canvas_lesson(readme, filepath, branch, name, type, dry_run, fis_links, remove_header_and_footer)
+  def update_canvas_lesson(course, markdown, filepath, branch, name, type, save_to_github, fis_links, remove_header_and_footer, only_update_content, id)
     GithubInterface.get_updated_repo(filepath, branch)
-    new_readme = RepositoryConverter.convert(filepath, readme, branch, remove_header_and_footer)
+    new_html = RepositoryConverter.convert(filepath, markdown, branch, remove_header_and_footer)
     if fis_links
-      new_readme = RepositoryConverter.add_fis_links(filepath, new_readme)
+      new_html = RepositoryConverter.add_fis_links(filepath, new_html) # adds Flatiron School specific header and footer
     end
-    canvas_data = CanvasDotfile.read_canvas_data
-    canvas_data[:lessons].each { |lesson|
-      CanvasInterface.update_existing_lesson(lesson[:course_id], lesson[:id], lesson[:type], name, new_readme, dry_run)
-    }
+    if !id
+      # If no assignment or page ID is provided, tries to get info from a .canvas file
+      canvas_data = CanvasDotfile.read_canvas_data
+      canvas_data[:lessons].each { |lesson|
+        response = CanvasInterface.update_existing_lesson(lesson[:course_id], lesson[:id], lesson[:type], name, new_html, only_update_content)
+        # puts "Canvas lesson created. Lesson available at #{response['html_url']}."
+      }
+    else
+      # If an ID is provided, uses the ID instead of the .canvas file
+      info = CanvasInterface.get_lesson_info(course, id)
+      CanvasInterface.update_existing_lesson(course, id, info[1], name, new_html, only_update_content)
+    end
   end
 
   
