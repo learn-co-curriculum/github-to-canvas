0b11ac58f0f40c032f5dbf9220efca8603bd5ab8
maxwellbenton <maxwellbenton@gmail.com>
2020-07-08 17:07:41 -0400

    adjust save functionality

--- lib/github-to-canvas/create_canvas_lesson.rb
+++ lib/github-to-canvas/create_canvas_lesson.rb
@@ -18,15 +18,21 @@ class CreateCanvasLesson
       new_html = RepositoryConverter.add_fis_links(filepath, new_html)
     end
     response = CanvasInterface.submit_to_canvas(course, type, name, new_html)
+    
+    puts 'Creating .canvas file'
+    CanvasDotfile.update_or_create(filepath, response, course, type) 
+
+    puts "Canvas lesson created. Lesson available at #{response['html_url']}."
+
+    # If --save option is used, the .canvas file gets committed and pushed to the remote repo
     if save_to_github
-      puts 'Creating .canvas file'
-      CanvasDotfile.update_or_create(filepath, response, course, type)
+      puts 'Adding .canvas file'
+      GithubInterface.git_add(filepath, '.canvas')
       puts 'Commiting .canvas file'
-      CanvasDotfile.commit_canvas_dotfile(filepath)
+      GithubInterface.git_commit(filepath, 'AUTO: add .canvas file after migration')
       puts 'Pushing .canvas file'
       GithubInterface.git_push(filepath, branch)
     end
-    puts "Canvas lesson created. Lesson available at #{response['html_url']}."
   end
 
 end
